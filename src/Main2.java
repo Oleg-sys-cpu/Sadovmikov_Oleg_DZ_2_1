cfb778e115806e70429884c85407a2f9e2617a13
U_24AKN <osadovnikov@alfabank.ru>
2025-08-14 00:51:19 +0400

    New Project

--- src/Main2.java
+++ src/Main2.java
@@ -12,7 +12,7 @@ public class Main2 {
         }
     }
 
-    public static void main2(String[] args) {
+    public static void main(String[] args) {
         checkSumSign(); // Вызов метода
     }
 }
