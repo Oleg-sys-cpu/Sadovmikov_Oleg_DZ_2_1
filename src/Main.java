7b99ef2906288fa905beb3ed743626ca87566da9
U_24AKN <osadovnikov@alfabank.ru>
2025-08-14 00:40:31 +0400

    New Project

--- src/Main.java
+++ src/Main.java
@@ -10,3 +10,22 @@ public class Main { // Или имя вашего класса
         printThreeWords(); // Вызов метода для печати слов
     }
 }
+
+public class Main2 { // Или имя вашего класса
+
+    public static void checkSumSign() {
+        int a = 10; // Инициализация переменной a любым целым числом
+        int b = -5; // Инициализация переменной b любым целым числом
+        int sum = a + b;
+
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void main2(String[] args) {
+        checkSumSign(); // Вызов метода
+    }
+}
