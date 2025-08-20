cb6a79e40a797b87ad319ff48cdec7a9e47a6b23
U_24AKN <osadovnikov@alfabank.ru>
2025-08-21 00:58:22 +0400

    New Project

--- lesson_2_4_New/src/Main1.java
+++ lesson_2_4_New/src/Main1.java
@@ -1,5 +1,3 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 // Базовый класс Животное
 class Animal {
     protected String name;
